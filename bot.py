dfc6f9a0cc5b4087079d4648ae6a4a37136aae3c
Michael <soplomiha@gmail.com>
2022-08-10 00:21:16 +0300

    setup DialogFlow

--- bot.py
+++ bot.py
@@ -1,7 +1,10 @@
 import os
+
 from dotenv import load_dotenv
+from google.cloud import dialogflow, storage
 from telegram import Update
-from telegram.ext import Updater, CommandHandler, MessageHandler, Filters, CallbackContext
+from telegram.ext import (CallbackContext, CommandHandler, Filters,
+                          MessageHandler, Updater)
 
 
 def start(update: Update, context: CallbackContext) -> None:
@@ -11,7 +14,28 @@ def start(update: Update, context: CallbackContext) -> None:
 
 def echo(update: Update, context: CallbackContext) -> None:
     """Echo the user message."""
-    update.message.reply_text(update.message.text)
+    session_id = update.effective_chat.id
+
+    update.message.reply_text(
+        detect_intent_texts(
+            os.getenv("PROJECT_ID"),
+            session_id,
+            update.message.text,
+            "ru-RU"
+        )
+    )
+
+
+def detect_intent_texts(project_id, session_id, text, language_code):
+    session_client = dialogflow.SessionsClient()
+
+    session = session_client.session_path(project_id, session_id)
+    text_input = dialogflow.TextInput(text=text, language_code=language_code)
+    query_input = dialogflow.QueryInput(text=text_input)
+    response = session_client.detect_intent(
+        request={"session": session, "query_input": query_input}
+    )
+    return response.query_result.fulfillment_text
 
 
 def main() -> None:
