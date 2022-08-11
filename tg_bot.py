838715181c2a7646d787d6a5aa05739a549f0705
Michael <soplomiha@gmail.com>
2022-08-11 17:47:21 +0300

    Refactory telegram bot script

--- tg_bot.py
+++ tg_bot.py
@@ -1,10 +1,11 @@
 import os
 
 from dotenv import load_dotenv
-from google.cloud import dialogflow, storage
 from telegram import Update
 from telegram.ext import (CallbackContext, CommandHandler, Filters,
                           MessageHandler, Updater)
+
+from dialogflow_api import detect_intent_texts
 
 
 def start(update: Update, context: CallbackContext) -> None:
@@ -15,10 +16,11 @@ def start(update: Update, context: CallbackContext) -> None:
 def echo(update: Update, context: CallbackContext) -> None:
     """Echo the user message."""
     session_id = update.effective_chat.id
+    dialogflow_project_id = context.bot_data['dialogflow_project_id']
 
     update.message.reply_text(
         detect_intent_texts(
-            os.getenv("PROJECT_ID"),
+            dialogflow_project_id,
             session_id,
             update.message.text,
             "ru-RU"
@@ -26,24 +28,15 @@ def echo(update: Update, context: CallbackContext) -> None:
     )
 
 
-def detect_intent_texts(project_id, session_id, text, language_code):
-    session_client = dialogflow.SessionsClient()
-
-    session = session_client.session_path(project_id, session_id)
-    text_input = dialogflow.TextInput(text=text, language_code=language_code)
-    query_input = dialogflow.QueryInput(text=text_input)
-    response = session_client.detect_intent(
-        request={"session": session, "query_input": query_input}
-    )
-    return response.query_result.fulfillment_text
-
-
 def main() -> None:
     """Start the bot."""
     load_dotenv()
     tg_token = os.getenv("TG_BOT_TOKEN")
+    dialogflow_project_id = os.getenv("DIALOGFLOW_PROJECT_ID")
+
     updater = Updater(tg_token)
     dispatcher = updater.dispatcher
+    dispatcher.bot_data['dialogflow_project_id'] = dialogflow_project_id
     dispatcher.add_handler(CommandHandler("start", start))
     dispatcher.add_handler(MessageHandler(
         Filters.text & ~Filters.command, echo))
