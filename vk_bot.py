f0bc8438115f57fc2352a9e7d6aff2b2176444ed
Michael <soplomiha@gmail.com>
2022-08-11 17:23:10 +0300

    Add echobot for VK

--- vk_bot.py
+++ vk_bot.py
@@ -1,18 +1,28 @@
 import os
-import vk_api
-from vk_api.longpoll import VkLongPoll, VkEventType
+import random
+
+import vk_api as vk
 from dotenv import load_dotenv
+from vk_api.longpoll import VkEventType, VkLongPoll
 
-load_dotenv()
-vk_token = os.getenv("VK_BOT_TOKEN")
-vk_session = vk_api.VkApi(token=vk_token)
-longpoll = VkLongPoll(vk_session)
 
-for event in longpoll.listen():
-    if event.type == VkEventType.MESSAGE_NEW:
-        print('Новое сообщение:')
-        if event.to_me:
-            print('Для меня от: ', event.user_id)
-        else:
-            print('От меня для: ', event.user_id)
-        print('Текст:', event.text)
+def echo(event, vk_api):
+    print(event.user_id)
+    print(event.text)
+    print(event)
+    vk_api.messages.send(
+        user_id=event.user_id,
+        message=event.text,
+        random_id=random.randint(1, 1000)
+    )
+
+
+if __name__ == "__main__":
+    load_dotenv()
+    vk_token = os.getenv("VK_BOT_TOKEN")
+    vk_session = vk.VkApi(token=vk_token)
+    longpoll = VkLongPoll(vk_session)
+    vk_api = vk_session.get_api()
+    for event in longpoll.listen():
+        if event.type == VkEventType.MESSAGE_NEW and event.to_me:
+            echo(event, vk_api)
