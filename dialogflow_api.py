29eca67434d83af04a3bbae3f846b3c7f892212f
Michael <soplomiha@gmail.com>
2022-08-15 22:52:41 +0300

    Remove exception handling

--- dialogflow_api.py
+++ dialogflow_api.py
@@ -7,24 +7,17 @@ logger = logging.getLogger('dialogflow')
 
 
 def detect_intent_texts(project_id, session_id, text, language_code):
-    try:
-        logger.debug('Request dialogflow')
-        session_client = dialogflow.SessionsClient()
+    logger.debug('Request dialogflow')
+    session_client = dialogflow.SessionsClient()
 
-        session = session_client.session_path(project_id, session_id)
-        text_input = dialogflow.TextInput(text=text, language_code=language_code)
-        query_input = dialogflow.QueryInput(text=text_input)
-        response = session_client.detect_intent(
-            request={"session": session, "query_input": query_input}
-        )
+    session = session_client.session_path(project_id, session_id)
+    text_input = dialogflow.TextInput(text=text, language_code=language_code)
+    query_input = dialogflow.QueryInput(text=text_input)
+    response = session_client.detect_intent(
+        request={"session": session, "query_input": query_input}
+    )
 
-        return {
-            'text': response.query_result.fulfillment_text,
-            'is_fallback': response.query_result.intent.is_fallback,
-        }
-    except Exception as err:
-        logger.exception('Error on detect_intent', exc_info=err)
-        return {
-            'text': '',
-            'is_fallback': True,
-        }
+    return {
+        'text': response.query_result.fulfillment_text,
+        'is_fallback': response.query_result.intent.is_fallback,
+    }
